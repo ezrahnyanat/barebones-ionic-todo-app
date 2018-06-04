1ac49c3d04b432389462049afd3ed7ff3561da10
Ezrah Nyanat <ezrahnyanat@gmail.com>
2018-06-04 01:41:13 -0500

    Add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database'
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB1QXdtvWKuEkTBTJHiXLO2BQNuxfc0NUs",
+  authDomain: "todolist-fcf8b.firebaseapp.com",
+  databaseURL: "https://todolist-fcf8b.firebaseio.com",
+  projectId: "todolist-fcf8b",
+  storageBucket: "todolist-fcf8b.appspot.com",
+  messagingSenderId: "699903704039"
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +38,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,118 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController, ActionSheetController} from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'
+import { Observable } from 'rxjs'
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  itemsRef: AngularFireList<any>
+  items: Observable<any[]>
+  
+  constructor(public navCtrl: NavController, 
+    public alertCtrl: AlertController,
+    public actionSheetCtrl: ActionSheetController,
+    db: AngularFireDatabase) {
+    this.itemsRef = db.list('/items')
+    this.items = this.itemsRef.valueChanges()
+  }
 
+  addItem() {
+    let prompt = this.alertCtrl.create({
+      title: 'Todo',
+      message: 'What do you need to do?',
+      inputs: [
+        {
+          name: 'Todo',
+          placeholder: 'Todo'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked')
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            const newItemRef = this.itemsRef.push({})
+
+            newItemRef.set({
+              id: newItemRef.key,
+              name: data.Todo
+            })
+          }
+        }
+      ]
+    })
+    prompt.present()
+  }
+
+  showOptions(itemId, itemName) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Delete or Update?',
+      buttons: [
+        {
+          text: 'Delete Item',
+          handler: () => {
+            this.removeItem(itemId)
+          }
+        },
+        {
+          text: 'Update Item',
+          handler: () => {
+            this.updateItem(itemId, itemName)
+          }
+        },
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Cancel clicked')
+          }
+        }
+      ]
+    })
+    actionSheet.present()
+  }
+
+  removeItem(itemId: string) {
+    this.itemsRef.remove(itemId)
+  }
+
+  updateItem(itemId, itemName){
+    let prompt = this.alertCtrl.create({
+      title: 'Item Name',
+      message: 'Update item',
+      inputs: [
+        {
+          name: 'todo',
+          placeholder: 'todo',
+          value: itemName
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel', 
+          handler: data => {
+            console.log('Cancel clicked')
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.itemsRef.update(itemId, {
+              name: data.name
+            })
+          }
+        }
+      ]
+    })
+    prompt.present()
   }
 
 }
